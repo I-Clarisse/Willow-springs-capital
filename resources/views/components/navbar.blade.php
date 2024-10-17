9e59238b601565895bf566af09f00569a002a993
K.Marvin <83425741+KarsMarvin@users.noreply.github.com>
2024-10-17 12:04:37 +0300

    contact

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,16 +1,16 @@
-<nav class="bg-black text-white py-4">
-    <div class="container mx-auto flex items-center justify-between">
-        <div class="flex items-center">
-            <img src="{{ asset('images/logo.png') }}" alt="Logo" class="h-12 mr-4">
-            <span class="font-bold text-lg">Willow Springs Capital</span>
+<nav class="bg-black bg-opacity-65 backdrop-blur-lg text-white py-4">
+    <div class="container mx-auto flex items-center">
+        <div class="flex items-center mr-8">
+            <img src="{{ asset('images/logo_transparent_background.png') }}" alt="Logo" class="h-16 mr-4"> <!-- Increased height -->
+            <!-- <span class="font-bold text-lg">Willow Springs Capital</span> -->
         </div>
-        <div class="flex space-x-6">
+        <div class="flex-grow flex justify-center space-x-8">
             <a href="/" class="hover:text-gray-300">Home</a>
             <a href="/about" class="hover:text-gray-300">About Us</a>
             <a href="#" class="hover:text-gray-300">Services</a>
             <a href="#" class="hover:text-gray-300">Projects</a>
             <a href="#" class="hover:text-gray-300">Blog</a>
-            <a href="#" class="hover:text-gray-300">Contact Us</a>
+            <a href="/contact" class="hover:text-gray-300">Contact Us</a>
         </div>
     </div>
 </nav>
